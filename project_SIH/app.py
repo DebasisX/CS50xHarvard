a00eff25a4f702bf4be4900f670fc8cc7ddf67c6
Debasis Sikdar <115938061+DebasisX@users.noreply.github.com>
2024-12-06 01:09:06 +0530

    Delete project_SIH directory

--- project_SIH/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, flash, redirect, render_template, request, session
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    if request.method == "GET":
-        return render_template("index.html")
